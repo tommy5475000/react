2f9db7de0901278913b27e73da5c308d71a5c1f2
Hà Đạt Cường <133970445+tommy5475000@users.noreply.github.com>
2023-09-17 18:31:43 +0700

    ip

--- src/BT-DatVe/Main.jsx
+++ src/BT-DatVe/Main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import data from "./danhSachGhe.json";
 import SeatList from "./SeatList";
 import Tickets from "./Tickets";
-import {useSelector} from "react-redux"
+import { useSelector } from "react-redux";
 
 export default function Main() {
-  const{totalPrice}=useSelector((state)=>{
-    return state.ticket
-  })
+  const { totalPrice } = useSelector((state) => {
+    return state.ticket;
+  });
   const handleBook = () => {
     alert(`Tổng tiền của bạn: ${totalPrice}VND`);
   };
@@ -47,57 +47,81 @@ export default function Main() {
               <h5 className="text-warning my-3">
                 Fill The Required Details Below And Select Your Seats
               </h5>
-              <div >
-                <ul style={{ listStyle: "none" }} className="mt-2">
+              <div>
+                <ul className="mt-2">
                   <li
                     style={{
                       textAlign: "left",
-                      marginRight: "16px",
                       color: "white",
                       display: "inline-block",
                     }}
                     className="me-5"
                   >
-                    Selected Seat
+                    <img
+                      style={{
+                        height: "15px",
+                        width: "15px",
+                        marginRight:"5px",
+                        verticalAlign:".1px",
+                      }}
+                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6zIYlwKXtRLMAtDXTzcIbgEoN2SCXZH_kT_1KLuqBm3QdBw_9ZmmbVcuam6bsIOs_tes&usqp=CAU"
+                      alt=""
+                    />
+                    Ghế đang chọn
                   </li>
                   <li
                     style={{
                       textAlign: "left",
-                      marginRight: "16px",
+                      
                       color: "white",
                       display: "inline-block",
                     }}
                     className="me-5"
                   >
-                    Reserved Seat
+                    <img
+                      style={{
+                        height: "15px",
+                        width: "15px",
+                        marginRight:"5px",
+                        verticalAlign:".1px",
+                      }}
+                      src="https://e7.pngegg.com/pngimages/329/173/png-clipart-red-computer-icons-square-s-angle-color-thumbnail.png"
+                      alt=""
+                    />
+                    Ghế đã đặt
                   </li>
                   <li
                     style={{
                       textAlign: "left",
-                      marginRight: "16px",
                       color: "white",
                       display: "inline-block",
-                      //   ':before':{
-                      //     content: "",
-                      //     width: "15px",
-                      //     height: "15px",
-                      //     display: "inline-block",
-                      //     marginRight: "10px",
-                      //   }
-                    }}
+                      }}
                     className="me-5"
                   >
-                    Empty Seat
+                      <img
+                        style={{
+                          height: "15px",
+                          width: "15px",
+                          marginRight:"5px",
+                          verticalAlign:".1px",
+                        }}
+                        src="https://printgo.vn/uploads/media/792227/cac-loai-mau-xanh-2_1624333127.jpg"
+                        alt=""
+                      />
+                    Ghế đang trống
                   </li>
                 </ul>
 
                 <div>
                   <SeatList seats={data} />
                 </div>
-                <button className="btn btn-dark mt-3" onClick={handleBook}>Đặt Vé</button>
+                <button className="btn btn-dark mt-3" onClick={handleBook}>
+                  Đặt Vé
+                </button>
 
-                <div><Tickets/></div>
-
+                <div>
+                  <Tickets />
+                </div>
               </div>
             </div>
           </div>
